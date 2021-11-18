cd5e2b4218ed16715c2d37eb439def510567aa5e
Helena Felicia de Oliveira Avelino <helenafeliaoliveira@gmail.com>
2021-11-17 23:10:03 -0300

    Adicionando telas

---
+++ Trabalho calculadora/assets/js/calc.js
@@ -0,0 +1,14 @@
+Menu
+var btMostrar = document.getElementById("Icone");
+	
+	document.getElementById("iMenu_esquerdo").style.display = "none";
+	
+	btMostrar.onclick = function(){
+		if(iMenu_esquerdo.style.display == "none"){
+			iMenu_esquerdo.style.display = "block";
+		}else{
+			iMenu_esquerdo.style.display = "none";
+	}
+		
+	}
+	
